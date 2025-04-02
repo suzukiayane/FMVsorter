ac21d245e79f8c2a578fb5f51f408ac6383a0a72
suzukiayane <61143291+suzukiayane@users.noreply.github.com>
2025-04-02 18:55:58 +0800

    Added a bunch of games
    Games Added:
    Charming Hearts
    Drive Me Crazy
    Exclusive Memory
    Game of Fate
    Game of Fate 2
    I've Fallen For You
    Master of Love
    My Destiny Girls
    Rebirth! New Lover
    Superscout
    Vanity Fair: The Pursuit

---
+++ src/js/data/2025-04-02.js
@@ -0,0 +1,628 @@
+dataSetVersion = "2025-04-02"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Game",
+    key: "groups",
+    tooltip: "Check this to restrict to certain games.",
+    checked: true,
+    sub: [
+	  { name: "Charming Hearts", key: "CH"},	
+	  { name: "Director Simulator", key: "DS"},
+	  { name: "Exclusive Memory", key: "EM"},
+	  { name: "Five Hearts Under One Roof", key: "FHUOR"},
+	  { name: "Game of Fate", key: "GoF"},
+	  { name: "Game of Fate 2", key: "GoF2"},
+	  { name: "Hello Love: 18 Again", key: "HL18A"}, 
+	  { name: "I've Fallen for You!", key: "IFfY"}, 
+      { name: "Love is All Around", key: "LiAA" },
+	  { name: "Love is All Around DLC", key: "LiAADLC"},
+	  { name: "Master of Love", key: "MoL"},
+	  { name: "My Destiny Girls", key: "MDG"},
+	  { name: "Rebirth! New Lover", key: "RNL"},
+	  { name: "Superscout", key: "Scout"},
+	  { name: "Vanity Fair: The Pursuit", key: "VFTP"}	  
+    ]
+  }
+];
+
+dataSet[dataSetVersion].characterData = [
+  //LiAA
+  {
+    name: "",
+    img: "4mSFkRZ.png",
+    opts: 
+	{
+      groups: ["LiAA"]
+    }
+  },
+  {
+    name: "",
+    img: "1ErBEOs.png",
+    opts: {
+      groups: ["LiAA"]
+    }
+  },
+  {
+    name: "",
+    img: "sQp6xrX.png",
+    opts: {
+      groups: ["LiAA"]
+    }
+  },
+  {
+    name: "",
+    img: "n1KgXdx.png",
+    opts: {
+      groups: ["LiAA"]
+    }
+  },
+  {
+    name: "",
+    img: "D0EeE0G.png",
+    opts: {
+      groups: ["LiAA"]
+    }
+  },
+  {
+    name: "",
+    img: "OExOGhf.png",
+    opts: {
+      groups: ["LiAA"]
+    }
+  },
+  //LiAA DLC
+  {
+    name: "",
+    img: "4bbKWjk.png",
+    opts: {
+      groups: ["LiAADLC"]
+    }
+  },
+  {
+    name: "",
+    img: "Ec4wWv3.png",
+    opts: {
+      groups: ["LiAADLC"]
+    }
+  },
+  {
+    name: "",
+    img: "8UcfwBB.png",
+    opts: {
+      groups: ["LiAADLC"]
+    }
+  },
+  //HL18A
+  {
+    name: "Su Xiaoxiao",
+    img: "6DIkjKB.png",
+    opts: {
+      groups: ["HL18A"]
+    }
+  },
+  {
+    name: "Xu Jiajia",
+    img: "6d5scqF.png",
+    opts: {
+      groups: ["HL18A"]
+    }
+  },
+  {
+    name: "Shen Mengxuan",
+    img: "sG40OpX.png",
+    opts: {
+      groups: ["HL18A"]
+    }
+  },
+  {
+    name: "Lu Yue Ru",
+    img: "quZXPCb.png",
+    opts: {
+      groups: ["HL18A"]
+    }
+  },
+  {
+    name: "Chu Yao",
+    img: "lE8hxEU.png",
+    opts: {
+      groups: ["HL18A"]
+    }
+  },
+  //DS
+  {
+    name: "",
+    img: "E7wcAfl.png",
+    opts: {
+      groups: ["DS"]
+    }
+  },
+  {
+    name: "",
+    img: "jH413F1.png",
+    opts: {
+      groups: ["DS"]
+    }
+  },
+  {
+    name: "",
+    img: "tz3wsGz.png",
+    opts: {
+      groups: ["DS"]
+    }
+  },
+  {
+    name: "",
+    img: "43IMW0r.png",
+    opts: {
+      groups: ["DS"]
+    }
+  },
+  {
+    name: "",
+    img: "7Yg7P51.png",
+    opts: {
+      groups: ["DS"]
+    }
+  },
+  {
+    name: "",
+    img: "Xsj87La.png",
+    opts: {
+      groups: ["DS"]
+    }
+  },
+  //FHUOR
+  {
+    name: "",
+    img: "esDmbq0.png",
+    opts: {
+      groups: ["FHUOR"]
+    }
+  },
+  {
+    name: "",
+    img: "7KEHYGB.png",
+    opts: {
+      groups: ["FHUOR"]
+    }
+  },
+  {
+    name: "",
+    img: "sfFAo9g.png",
+    opts: {
+      groups: ["FHUOR"]
+    }
+  },
+  {
+    name: "",
+    img: "Nt7mYTK.png",
+    opts: {
+      groups: ["FHUOR"]
+    }
+  },
+  {
+    name: "",
+    img: "VCb0cwg.png",
+    opts: {
+      groups: ["FHUOR"]
+    }
+  },
+  //CH
+  {
+    name: "",
+    img: "vgefCZ2.png",
+    opts: {
+      groups: ["CH"]
+    }
+  },
+  {
+    name: "",
+    img: "NcvzTYH.png",
+    opts: {
+      groups: ["CH"]
+    }
+  },
+  {
+    name: "",
+    img: "t4mUG92.png",
+    opts: {
+      groups: ["CH"]
+    }
+  },
+  {
+    name: "",
+    img: "JveSjmu.png",
+    opts: {
+      groups: ["CH"]
+    }
+  },
+  {
+    name: "",
+    img: "mAw5ETO.png",
+    opts: {
+      groups: ["CH"]
+    }
+  },
+  {
+    name: "",
+    img: "6DLNDkW.png",
+    opts: {
+      groups: ["CH"]
+    }
+  },
+  //Exclusive Memory  
+  {
+    name: "",
+    img: "2hnaOJo.png",
+    opts: {
+      groups: ["EM"]
+    }
+  },
+  {
+    name: "",
+    img: "mPVHymY.png",
+    opts: {
+      groups: ["EM"]
+    }
+  },
+  {
+    name: "",
+    img: "0o6xLTg.png",
+    opts: {
+      groups: ["EM"]
+    }
+  },
+  {
+    name: "",
+    img: "quWR94e.png",
+    opts: {
+      groups: ["EM"]
+    }
+  },
+  {
+    name: "",
+    img: "xPQdu9H.png",
+    opts: {
+      groups: ["EM"]
+    }
+  },
+  {
+    name: "",
+    img: "A5ZdzHD.png",
+    opts: {
+      groups: ["EM"]
+    }
+  },
+  //Game of Fate
+  {
+    name: "Cheng Linqiang",
+    img: "Wt7idAt.png",
+    opts: {
+      groups: ["GoF"]
+    }
+  },
+  {
+    name: "Gu Siqi",
+    img: "FCcIfpd.png",
+    opts: {
+      groups: ["GoF"]
+    }
+  },
+  {
+    name: "Cheng Linwei",
+    img: "1Tv3P9y.png",
+    opts: {
+      groups: ["GoF"]
+    }
+  },
+  {
+    name: "Jiang Man",
+    img: "o0jOl7y.png",
+    opts: {
+      groups: ["GoF"]
+    }
+  },
+  {
+    name: "Qiu Shuya",
+    img: "7mEvESp.png",
+    opts: {
+      groups: ["GoF"]
+    }
+  },
+  //Game of Fate 2
+  {
+    name: "Cheng Yuesheng",
+    img: "DSxDHIZ.png",
+    opts: {
+      groups: ["GoF2"]
+    }
+  },
+  {
+    name: "Fang Zhilian",
+    img: "KwEFwqN.png",
+    opts: {
+      groups: ["GoF2"]
+    }
+  },
+  {
+    name: "Gu Xinfan",
+    img: "G4ob4mw.png",
+    opts: {
+      groups: ["GoF2"]
+    }
+  },
+  {
+    name: "Ye Kui",
+    img: "dZQ2OXW.png",
+    opts: {
+      groups: ["GoF2"]
+    }
+  },
+  {
+    name: "Yoko Nakamura",
+    img: "sNM07uf.png",
+    opts: {
+      groups: ["GoF2"]
+    }
+  },
+  //I've Fallen For You!
+  {
+    name: "",
+    img: "fVFy6Kp.png",
+    opts: {
+      groups: ["IFfY"]
+    }
+  },
+  {
+    name: "",
+    img: "mkjEQOc.png",
+    opts: {
+      groups: ["IFfY"]
+    }
+  },
+  {
+    name: "",
+    img: "XE9HkdD.png",
+    opts: {
+      groups: ["IFfY"]
+    }
+  },
+  {
+    name: "",
+    img: "AwUk1Y9.png",
+    opts: {
+      groups: ["IFfY"]
+    }
+  },
+  {
+    name: "",
+    img: "BRqGrqY.png",
+    opts: {
+      groups: ["IFfY"]
+    }
+  },
+  {
+    name: "",
+    img: "ddkPN1B.png",
+    opts: {
+      groups: ["IFfY"]
+    }
+  },
+  {
+    name: "",
+    img: "9PTYAVc.png",
+    opts: {
+      groups: ["IFfY"]
+    }
+  },
+  {
+    name: "",
+    img: "PMiVhaf.png",
+    opts: {
+      groups: ["IFfY"]
+    }
+  },
+  //Master of Love
+  {
+    name: "",
+    img: "WEpx3K9.png",
+    opts: {
+      groups: ["MoL"]
+    }
+  },
+  {
+    name: "",
+    img: "n0jrNSh.png",
+    opts: {
+      groups: ["MoL"]
+    }
+  },
+  {
+    name: "",
+    img: "xdhJhHo.png",
+    opts: {
+      groups: ["MoL"]
+    }
+  },
+  {
+    name: "",
+    img: "u1wfsKY.png",
+    opts: {
+      groups: ["MoL"]
+    }
+  },
+  {
+    name: "",
+    img: "arNVW2b.png",
+    opts: {
+      groups: ["MoL"]
+    }
+  },
+  //My Destiny Girls
+  {
+    name: "Gui Sihan",
+    img: "Wm4rzJ5.png",
+    opts: {
+      groups: ["MDG"]
+    }
+  },
+  {
+    name: "He Yuxiao",
+    img: "sEbn4SF.png",
+    opts: {
+      groups: ["MDG"]
+    }
+  },
+  {
+    name: "Lisa",
+    img: "Ad77Zrh.png",
+    opts: {
+      groups: ["MDG"]
+    }
+  },
+  {
+    name: "Lu Xiaoyue",
+    img: "vEZFlzw.png",
+    opts: {
+      groups: ["MDG"]
+    }
+  },
+  {
+    name: "Shao Na",
+    img: "QOchSbm.png",
+    opts: {
+      groups: ["MDG"]
+    }
+  },
+  {
+    name: "Zhuang Shiying",
+    img: "D5c91Rd.png",
+    opts: {
+      groups: ["MDG"]
+    }
+  },
+  //Rebirth New Lover
+  {
+    name: "Gu Qiuhan",
+    img: "m9PGDOB.png",
+    opts: {
+      groups: ["RNL"]
+    }
+  },
+  {
+    name: "Lin Miaoxian",
+    img: "GSuuaJ5.png",
+    opts: {
+      groups: ["RNL"]
+    }
+  },
+  {
+    name: "Mingyue Rao",
+    img: "mUTlvu7.png",
+    opts: {
+      groups: ["RNL"]
+    }
+  },
+  {
+    name: "Qing'er",
+    img: "tWiCwJW.png",
+    opts: {
+      groups: ["RNL"]
+    }
+  },
+  {
+    name: "Xiaobai",
+    img: "POsI60Q.png",
+    opts: {
+      groups: ["RNL"]
+    }
+  },
+  //Superscout
+  {
+    name: "Duoduo",
+    img: "cy1DrHu.png",
+    opts: {
+      groups: ["Scout"]
+    }
+  },
+  {
+    name: "Lin Jia",
+    img: "DvYjhqZ.png",
+    opts: {
+      groups: ["Scout"]
+    }
+  },
+  {
+    name: "Lin Qian'er",
+    img: "xrJp2HC.png",
+    opts: {
+      groups: ["Scout"]
+    }
+  },
+  {
+    name: "Tang Xiaolan",
+    img: "0gSSYtR.png",
+    opts: {
+      groups: ["Scout"]
+    }
+  },
+  {
+    name: "Ye Shu",
+    img: "yfMQw1L.png",
+    opts: {
+      groups: ["Scout"]
+    }
+  },
+  {
+    name: "Zhou Yang",
+    img: "OD4SGHp.png",
+    opts: {
+      groups: ["Scout"]
+    }
+  },
+  //Vanity Fair
+  {
+    name: "",
+    img: "HUfEiJq.png",
+    opts: {
+      groups: ["VFTP"]
+    }
+  },
+  {
+    name: "",
+    img: "dwIQMY2.png",
+    opts: {
+      groups: ["VFTP"]
+    }
+  },
+  {
+    name: "",
+    img: "RbfY6Al.png",
+    opts: {
+      groups: ["VFTP"]
+    }
+  },
+  {
+    name: "",
+    img: "xGZDFLh.png",
+    opts: {
+      groups: ["VFTP"]
+    }
+  },
+  {
+    name: "",
+    img: "7Vd8Kks.png",
+    opts: {
+      groups: ["VFTP"]
+    }
+  },
+  {
+    name: "",
+    img: "1WOEIew.png",
+    opts: {
+      groups: ["VFTP"]
+    }
+  }
+]
